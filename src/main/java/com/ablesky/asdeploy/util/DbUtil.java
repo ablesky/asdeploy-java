3b85f1fde2fe74b226cb9474f43432f40fc0fd68
zyang <zyang@ablesky.com>
2014-03-17 18:54:00 +0800

    添加用maven自动初始化开发环境数据库的功能

---
+++ src/main/java/com/ablesky/asdeploy/util/DbUtil.java
@@ -0,0 +1,31 @@
+package com.ablesky.asdeploy.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.SystemUtils;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DbUtil {
+
+	private DbUtil() {}
+	
+	/**
+	 * 不清楚<jdbc:initialize-database>是如何实现的
+	 */
+	@SuppressWarnings("resource")
+	public static final void initDb() throws IOException {
+		File dbFolder = new File(FilenameUtils.concat(SystemUtils.USER_DIR, "db"));
+		if(!dbFolder.exists()) {
+			dbFolder.mkdirs();
+		}
+		System.setProperty("spring.profiles.active", "development-init");
+		new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
+	
+	public static void main(String[] args) throws IOException {
+		initDb();
+	}
+
+}
