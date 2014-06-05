aa1ba20a1849c4a2386124e381e56949d7a3f495
zyang <zyang@ablesky.com>
2014-06-05 15:18:48 +0800

    去除一些编译过程中的警告

--- src/main/java/com/ablesky/asdeploy/util/SQLiteDialect.java
+++ src/main/java/com/ablesky/asdeploy/util/SQLiteDialect.java
@@ -72,10 +72,12 @@ public class SQLiteDialect extends Dialect {
 		return "select last_insert_rowid()";
 	}
 
+	@Deprecated
 	public boolean supportsLimit() {
 		return true;
 	}
 
+	@Deprecated
 	public String getLimitString(String query, boolean hasOffset) {
 		return new StringBuffer(query.length() + 20).append(query).append(
 				hasOffset ? " limit ?, ?" : " limit ?").toString();
